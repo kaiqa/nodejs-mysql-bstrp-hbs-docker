7dd538c8db78314f1027fa9bc4866c4c7b914086
kai <kai@kai.local>
2020-12-09 13:21:53 +0100

    added support for https

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const path = require('path');
 const app = express();
 const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
+const fs = require('fs');
+const options = {
+  key: fs.readFileSync('./cert/localhost.key'),
+  cert: fs.readFileSync('./cert/localhost.cert')
+};
+var https = require('https');
+var http = require('http');
+var servHttps = https.createServer(options, app);
+var servHttp = http.createServer(options, app);
+
 
 dotenv.config({ path: './.env' });
 const db = require('./model/db');
@@ -31,6 +41,12 @@ db.start.connect(function(err) {
 app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
 
-app.listen(80, () => {
-  console.log("listening on port 80");
+servHttps.listen(8443, () => {
+  console.log("listening on port 8443");
 })
+servHttp.listen(8080, () => {
+  console.log("listening on port 8080 ");
+})
+// app.listen(8080, () => {
+//   console.log("listening on port 8080");
+// })
