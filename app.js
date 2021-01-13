5467555c0c0cd00197577c411908b732d9967cce
kai <kai@kai-fluid.local>
2021-01-13 09:02:01 +0100

    play system sound on login, register, failed log in

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ const certificate = fs.readFileSync('./cert/cert.pem', 'utf8');
 const ca = fs.readFileSync('./cert/fullchain.pem', 'utf8');
 
 const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
+    key: privateKey,
+    cert: certificate,
+    ca: ca
 };
 
 var https = require('https');
@@ -25,6 +25,7 @@ var servHttp = http.createServer(credentials, app);
 
 dotenv.config({ path: './.env' });
 const db = require('./model/db');
+const { exec } = require('child_process');
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({ extended: false }));
@@ -37,15 +38,15 @@ app.set('view engine', 'hbs');
 const publicDirectoryPath = path.join(__dirname, "./public");
 app.use(express.static(publicDirectoryPath));
 
-db.start.connect(function(err) {
-  if(err) {
-    console.log(err);
-    console.log('Error connecting to the database');
-    debug('Error connecting to the database');
-  } else {
-    console.log('Connected to MYSQL');
-    debug('Connected to MYSQL');
-  }
+db.start.connect(function (err) {
+    if (err) {
+        console.log(err);
+        console.log('Error connecting to the database');
+        debug('Error connecting to the database');
+    } else {
+        console.log('Connected to MYSQL');
+        debug('Connected to MYSQL');
+    }
 });
 
 //Define Routes
@@ -53,25 +54,29 @@ app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
 
 servHttps.listen(443, () => {
-  console.log("listening on port 443");
-  debug("listening on port 443");
+    console.log("listening on port 443");
+    debug("listening on port 443");
 })
 servHttp.listen(80, () => {
-  console.log("listening on port 80 ");
-  debug("listening on port 80");
+    console.log("listening on port 80 ");
+    debug("listening on port 80");
 })
 // read files
 var directoryPath = './public/audio';
 //passsing directoryPath and callback function
 fs.readdir(directoryPath, function (err, files) {
-  //handling error
-  if (err) {
-      return console.log('Unable to scan directory: ' + err);
-  } 
-  //listing all files using forEach
-  files.forEach(function (file) {
-      // Do whatever you want to do with the file
-      console.log(file); 
-      debug(file);
-  });
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        //   console.log("\007");
+        //  console.log("say " + file);
+        // exec("say " + file);
+        //  exec('afplay /System/Library/Sounds/Ping.aiff')
+        console.log(file);
+        debug(file);
+    });
 });
--- controllers/auth.js
+++ controllers/auth.js
@@ -2,146 +2,150 @@ const jwt = require('jsonwebtoken');
 const db = require('../model/db');
 const bcrypt = require('bcryptjs');
 const { promisify } = require('util');
+const { exec } = require("child_process");
 
 exports.login = async (req, res, next) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  // 1) Check if email and password exist
-  if (!email || !password) {
-    return res.status(400).render("login", {
-      message: 'Please provide email and password'
-    });
-  }
-
-  // 2) Check if user exists && password is correct
-  db.start.query('SELECT * FROM users WHERE email = ?', [email], async (error, results) => {
-    console.log(results);
-    console.log(password);
-    // const isMatch = await bcrypt.compare(password, results[0].password);
-    // console.log(isMatch);
-    // if(!results || !isMatch ) {
-      // the above throws an async promise error when 
-     
-      if(!results[0]) {
-      return res.status(401).render("login", {
-        message: 'Incorrect email or password'
-      });
-    } 
-    
-    else  { 
-      const isMatch = await bcrypt.compare(password, results[0].password);
-      console.log(' password match = ' + isMatch)
-      if (isMatch == true){
-       // 3) If everything ok, send token to client
-       const id = results[0].id;
-       console.log(id);
-       const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-       expiresIn: process.env.JWT_EXPIRES_IN });
-       const cookieOptions = {
-        expires: new Date(
-          Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true
-                    };
-      res.cookie('jwt', token, cookieOptions);
-      res.status(200).redirect("/");
+    // 1) Check if email and password exist
+    if (!email || !password) {
+        return res.status(400).render("login", {
+            message: 'Please provide email and password'
+        });
     }
-          }
-     console.log('password invalid');
-     return res.status(401).render("login", {
-      message: 'Incorrect email or password'
+
+    // 2) Check if user exists && password is correct
+    db.start.query('SELECT * FROM users WHERE email = ?', [email], async (error, results) => {
+        console.log(results);
+        console.log(password);
+        // const isMatch = await bcrypt.compare(password, results[0].password);
+        // console.log(isMatch);
+        // if(!results || !isMatch ) {
+        // the above throws an async promise error when 
+
+        if (!results[0]) {
+            exec("say Incorrect email or password");
+            return res.status(401).render("login", {
+                message: 'Incorrect email or password'
+            });
+        }
+
+        else {
+            const isMatch = await bcrypt.compare(password, results[0].password);
+            console.log(' password match = ' + isMatch)
+            if (isMatch == true) {
+                // 3) If everything ok, send token to client
+                const id = results[0].id;
+                console.log(id);
+                const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+                    expiresIn: process.env.JWT_EXPIRES_IN
+                });
+                const cookieOptions = {
+                    expires: new Date(
+                        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+                    ),
+                    httpOnly: true
+                };
+                res.cookie('jwt', token, cookieOptions);
+                exec("say user is logged  in");
+                res.status(200).redirect("/");
+            }
+        }
+        console.log('password invalid');
+        return res.status(401).render("login", {
+            message: 'Incorrect email or password'
+        });
+
     });
-      
-  });
 };
 
 exports.register = (req, res) => {
-  console.log(req.body);
-  const { name, email, password, passwordConfirm } = req.body;
+    console.log(req.body);
+    const { name, email, password, passwordConfirm } = req.body;
 
-  // 2) Check if user exists && password is correct
-  db.start.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => {
-    if(error) {
-      console.log(error)
-    }
+    // 2) Check if user exists && password is correct
+    db.start.query('SELECT email FROM users WHERE email = ?', [email], async (error, results) => {
+        if (error) {
+            console.log(error)
+        }
 
-    if(results.length > 0 ) {
-      return res.render('register', {
+        if (results.length > 0) {
+            return res.render('register', {
                 message: 'That Email has been taken'
-              });
-    } else if(password !== passwordConfirm || password == 0) {
-      return res.render('register', {
-        message: 'Passwords do not match'
-      });
-    }
-      
-    let hashedPassword = await bcrypt.hash(password, 8);
-    console.log(hashedPassword);
+            });
+        } else if (password !== passwordConfirm || password == 0) {
+            return res.render('register', {
+                message: 'Passwords do not match'
+            });
+        }
 
-    db.start.query('INSERT INTO users SET ?', { name: name, email: email, password: hashedPassword }, (error, result) => {
-      if(error) {
-        console.log(error)
-      } else {
-        db.start.query('SELECT id FROM users WHERE email = ?', [email], (error, result) => {
-          const id = result[0].id;
-          console.log(id);
-          const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-            expiresIn: process.env.JWT_EXPIRES_IN
-          });
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
 
-          const cookieOptions = {
-            expires: new Date(
-              Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-            ),
-            httpOnly: true
-          };
-          res.cookie('jwt', token, cookieOptions);
+        db.start.query('INSERT INTO users SET ?', { name: name, email: email, password: hashedPassword }, (error, result) => {
+            if (error) {
+                console.log(error)
+            } else {
+                db.start.query('SELECT id FROM users WHERE email = ?', [email], (error, result) => {
+                    const id = result[0].id;
+                    console.log(id);
+                    const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+                        expiresIn: process.env.JWT_EXPIRES_IN
+                    });
 
-          res.status(201).redirect("/");
+                    const cookieOptions = {
+                        expires: new Date(
+                            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+                        ),
+                        httpOnly: true
+                    };
+                    res.cookie('jwt', token, cookieOptions);
+                    exec("say new user registered");
+                    res.status(201).redirect("/");
+                });
+            }
         });
-      }
     });
-  });
 };
 
 // Only for rendered pages, no errors!
 exports.isLoggedIn = async (req, res, next) => {
-  console.log(req.cookies);
-  if (req.cookies.jwt) {
-    try {
-      // 1) verify token
-      const decoded = await promisify(jwt.verify)(
-        req.cookies.jwt,
-        process.env.JWT_SECRET
-      );
+    console.log(req.cookies);
+    if (req.cookies.jwt) {
+        try {
+            // 1) verify token
+            const decoded = await promisify(jwt.verify)(
+                req.cookies.jwt,
+                process.env.JWT_SECRET
+            );
 
-      console.log("decoded");
-      console.log(decoded);
+            console.log("decoded");
+            console.log(decoded);
 
-      // 2) Check if user still exists
-      db.start.query('SELECT * FROM users WHERE id = ?', [decoded.id], (error, result) => {
-        console.log(result)
-        if(!result) {
-          return next();
+            // 2) Check if user still exists
+            db.start.query('SELECT * FROM users WHERE id = ?', [decoded.id], (error, result) => {
+                console.log(result)
+                if (!result) {
+                    return next();
+                }
+                // THERE IS A LOGGED IN USER
+                req.user = result[0];
+                // res.locals.user = result[0];
+                console.log("next")
+                return next();
+            });
+        } catch (err) {
+            return next();
         }
-        // THERE IS A LOGGED IN USER
-        req.user = result[0];
-        // res.locals.user = result[0];
-        console.log("next")
-        return next();
-      });
-    } catch (err) {
-      return next();
+    } else {
+        next();
     }
-  } else {
-    next();
-  }
 };
 
 exports.logout = (req, res) => {
-  res.cookie('jwt', 'loggedout', {
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true
-  });
-  res.status(200).redirect("/");
+    res.cookie('jwt', 'loggedout', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    });
+    res.status(200).redirect("/");
 };
